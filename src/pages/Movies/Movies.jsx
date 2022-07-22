aa73703db82b307a2298f87618582ea277146f26
PlatinumKellerman <platinumm@ukr.net>
2022-07-22 14:18:09 +0300

    in-progress

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -2,11 +2,21 @@ import { Formik, Field } from 'formik';
 import * as yup from 'yup';
 
 export function Movies() {
+  const schema = yup.object().shape({
+    movieName: yup.string().required('This field cannot be empty'),
+  });
+
   return (
     <>
-      <Formik>
+      <Formik initialValues={{ movieName: '' }} validationSchema={schema}>
         <form>
-          <Field />
+          <Field
+            name="movieName"
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search movies"
+          />
           <button></button>
         </form>
       </Formik>
