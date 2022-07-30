19b1a3c972e15b7a1a61e1d2e1497392cad2e116
PlatinumKellerman <platinumm@ukr.net>
2022-07-30 15:06:27 +0300

    goBackLink fix

--- src/components/MovieActions/MovieActions.jsx
+++ src/components/MovieActions/MovieActions.jsx
@@ -6,17 +6,19 @@ import {
 } from './MovieActions.styled';
 
 export function MovieActions({ location }) {
+  const backLinkHref = location.state?.from ?? '/movies';
+
   return (
     <AddInfoWrapper>
       <h3>Additional information</h3>
       <AddInfoList>
         <AddInfoItem>
-          <AddInfoLink to="credits" state={{ from: location }}>
+          <AddInfoLink to={'credits'} state={{ from: backLinkHref }}>
             {'>'} Cast
           </AddInfoLink>
         </AddInfoItem>
         <AddInfoItem>
-          <AddInfoLink to="reviews" state={{ from: location }}>
+          <AddInfoLink to="reviews" state={{ from: backLinkHref }}>
             {'>'} Reviews
           </AddInfoLink>
         </AddInfoItem>
